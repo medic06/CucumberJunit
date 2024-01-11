be1920569d3a117a036058559b272324a3e10aaa
medic06 <jim.hyland@comcast.net>
2024-01-11 14:50:45 -0700

    Commit cucumber Junit Framework

---
+++ src/test/java/cucumberOptions/JunitTestRunnerTest.java
@@ -0,0 +1,15 @@
+package cucumberOptions;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/java/features", glue = "stepDefinitions", monochrome = true, plugin = {
+		"html:target/cucumber.html", "json:target/json/cucumber.json",
+		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
+public class JunitTestRunnerTest {
+
+	
+}
